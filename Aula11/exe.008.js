604d7e2fb8891ea724709adbe943fa2eac31c0b1
Isabel Baptista Barros <isabelbaptista352@gmail.com>
2026-04-28 17:08:30 +0100

    Atualizações

---
+++ Aula11/exe.008.js
@@ -0,0 +1,6 @@
+var vel = 121
+console.log(`A velocidade do seu carro é ${vel} km por hora`)
+if(vel>60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando o cinto de segurança')
